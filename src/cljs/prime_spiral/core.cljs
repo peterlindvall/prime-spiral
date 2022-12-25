f3dca5b3f7369f4239507befdc1f9f2d3992d528
Peter Lindvall <peter.lindvall@gmail.com>
2022-12-25 23:39:04 +0100

    No navigation. Canvas covers available space

--- src/cljs/prime_spiral/core.cljs
+++ src/cljs/prime_spiral/core.cljs
@@ -1,50 +1,121 @@
 (ns prime-spiral.core
   (:require
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]
-   [reagent.session :as session]
-   [reitit.frontend :as reitit]
-   [clerk.core :as clerk]
-   [accountant.core :as accountant]))
+    [reagent.core :as reagent :refer [atom]]
+    [reagent.dom :as rdom]
+    [reagent.session :as session]
+    [reitit.frontend :as reitit]
+    [clerk.core :as clerk]
+    [accountant.core :as accountant]
+   ))
+
+(declare div-with-canvas)
+(declare draw-canvas-contents)
+(declare home-page)
+(def window-width (reagent/atom nil))
+(defn on-window-resize [ evt ]
+  (reset! window-width (.-innerWidth js/window)))
+
+(defn ^:export main []
+  (rdom/render [home-page]
+                  (.getElementById js/document "app"))
+  (.addEventListener js/window "resize" on-window-resize))
 
 ;; -------------------------
 ;; Routes
 
-(def router
+  (def router
   (reitit/router
-   [["/" :index]
-    ["/items"
-     ["" :items]
-     ["/:item-id" :item]]
-    ["/about" :about]]))
+    [["/" :index]
+     ["/items"
+      ["" :items]
+      ["/:item-id" :item]]
+     ["/about" :about]]))
 
-(defn path-for [route & [params]]
-  (if params
-    (:path (reitit/match-by-name router route params))
-    (:path (reitit/match-by-name router route))))
 
+  (defn path-for [route & [params]]
+    (if params
+      (:path (reitit/match-by-name router route params))
+      (:path (reitit/match-by-name router route))))
 ;; -------------------------
 ;; Page components
 
 (defn home-page []
   (fn []
-    [:span.main
-     [:h1 "Welcome to prime-spiral"]
-     [:ul
-      [:li [:a {:href (path-for :items)} "Items of prime-spiral"]]
-      [:li [:a {:href "/broken/link"} "Broken link"]]]]))
+    ;[:span.main
+    ; [:h1 "Welcome to prime-spiral"]
+    ; [:ul
+    ;  [:li [:a {:href (path-for :items)} "Items of prime-spiral"]]
+    ;  [:li [:a {:href "/broken/link"} "Broken link"]]]
+     [div-with-canvas]
+     ;]
+))
 
+(comment
+  (defn Canvas [{:keys [width height render]}]
+  (let [state (atom nil)]
+    (reagent/create-class
+      {:reagent-render      (fn []
+                              (let [update-size (fn [el]
+                                                  (when el
+                                                    (let [size (.get-real-size el)
+                                                          ctx (.getContext el "2d")]
+                                                      (swap! state assoc :size size)
+                                                      (render ctx size))))]
+                                (fn [] (let [{:keys [size]} @state]
+                                         [:canvas {:style  {:width width :height height}
+                                                   :ref    update-size
+                                                   :width  (nth size 0)
+                                                   :height (nth size 1)}]))))
+       :component-did-mount (fn [] (reset! state {:size nil}))
+       })))
+)
 
+(defn div-with-canvas [ ]
+  (let [dom-node (reagent/atom nil)]
+    (reagent/create-class
+      {:component-did-update
+       (fn [ this ]
+         (draw-canvas-contents (.-firstChild @dom-node)))
 
-(defn items-page []
-  (fn []
-    [:span.main
-     [:h1 "The items of prime-spiral"]
-     [:ul (map (fn [item-id]
-                 [:li {:name (str "item-" item-id) :key (str "item-" item-id)}
-                  [:a {:href (path-for :item {:item-id item-id})} "Item: " item-id]])
-               (range 1 60))]]))
+       :component-did-mount
+       (fn [ this ]
+         (reset! dom-node (reagent.dom/dom-node this))
+         (println "component-did-mount fn called")
+         )
 
+       :reagent-render
+       (fn [ ]
+         (println "reagent-render fn called")
+         @window-width ;; Trigger re-render on window resizes
+         [:div.with-canvas
+          ;; reagent-render is called before the compoment mounts, so
+          ;; protect against the null dom-node that occurs on the first render
+          [:canvas (if-let [ node @dom-node ]
+                     {:width (.-clientWidth node)
+                      :height (.-clientHeight node)})]])})))
+
+(defn draw-canvas-contents [ canvas ]
+  (let [ ctx (.getContext canvas "2d")
+        w (.-clientWidth canvas)
+        h (.-clientHeight canvas)]
+    (.beginPath ctx)
+    (.moveTo ctx 0 0)
+    (.lineTo ctx w h)
+    (.moveTo ctx w 0)
+    (.lineTo ctx 0 h)
+    (.stroke ctx))
+  (println "draw-canvas-contents -.clientHeight=" window-width))
+
+(comment
+  (defn items-page []
+    (fn []
+      [:span.main
+       [:h1 "The items of prime-spiral"]
+       [:ul (map (fn [item-id]
+                   [:li {:name (str "item-" item-id) :key (str "item-" item-id)}
+                    [:a {:href (path-for :item {:item-id item-id})} "Item: " item-id]])
+                 (range 1 11))]]))
+  )
 
 (defn item-page []
   (fn []
@@ -54,7 +125,6 @@
        [:h1 (str "Item " item " of prime-spiral")]
        [:p [:a {:href (path-for :items)} "Back to the list of items"]]])))
 
-
 (defn about-page []
   (fn [] [:span.main
           [:h1 "About prime-spiral"]]))
@@ -62,29 +132,30 @@
 
 ;; -------------------------
 ;; Translate routes -> page components
-
 (defn page-for [route]
   (case route
     :index #'home-page
-    :about #'about-page
-    :items #'items-page
-    :item #'item-page))
-
+    ;:about #'about-page
+    ;:items #'items-page
+    ;:item #'item-page
+))
 
 ;; -------------------------
 ;; Page mounting component
-
 (defn current-page []
   (fn []
     (let [page (:current-page (session/get :route))]
-      [:div
-       [:header
-        [:p [:a {:href (path-for :index)} "Home"] " | "
-         [:a {:href (path-for :about)} "About prime-spiral"]]]
+      ;[:div
+      ; [:header
+        ;[:p [:a {:href (path-for :index)} "Home"] " | "
+         ;[:a {:href (path-for :about)} "About prime-spiral"]]
+      ;  ]
        [page]
-       [:footer
-        [:p "prime-spiral was generated by the "
-         [:a {:href "https://github.com/reagent-project/reagent-template"} "Reagent Template"] "."]]])))
+      ; [:footer
+        ;[:p "prime-spiral was generated by the "
+        ; [:a {:href "https://github.com/reagent-project/reagent-template"} "Reagent Template"] "."]
+        ;]]
+)))
 
 ;; -------------------------
 ;; Initialize app
@@ -95,21 +166,20 @@
 (defn init! []
   (clerk/initialize!)
   (accountant/configure-navigation!
-   {:nav-handler
-    (fn [path]
-      (let [match (reitit/match-by-path router path)
-            current-page (:name (:data  match))
-            route-params (:path-params match)]
-        (reagent/after-render clerk/after-render!)
-        (session/put! :route {:current-page (page-for current-page)
-                              :route-params route-params})
-        (clerk/navigate-page! path)
-        ))
-    :path-exists?
-    (fn [path]
-      (boolean (reitit/match-by-path router path)))})
+    {:nav-handler
+     (fn [path]
+       (let [match (reitit/match-by-path router path)
+             current-page (:name (:data match))
+             route-params (:path-params match)]
+         (reagent/after-render clerk/after-render!)
+         (session/put! :route {:current-page (page-for current-page)
+                               :route-params route-params})
+         (clerk/navigate-page! path)
+         ))
+     :path-exists?
+     (fn [path]
+       (boolean (reitit/match-by-path router path)))})
   (accountant/dispatch-current!)
   (mount-root))
-
 (defn ^:dev/after-load reload! []
   (mount-root))
